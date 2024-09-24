8d35be9bf9f468e0d73e18b0e3b619a17df01cb3
Eduardo <eduardo25082002@gmail.com>
2024-09-24 17:53:59 -0300

    Commits Final

--- tipos-variaveis/src/SmartTv.java
+++ tipos-variaveis/src/SmartTv.java
@@ -1,37 +1,52 @@
-// Exercício de Java
 public class SmartTv {
-    // Estado Inicial da Smart TV
+    // Atributos
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
 
-    public void aumentarVolume(){
+    // Métodos que manipulam esses estados
 
-        //volume = volume + 1;
-        volume ++;
+    // método para o usuário mudar de canal com um parâmetro
+    public void mudarCanal(int novoCanal){ // atarvés dessa declaração o usuário pode informar qual canal ele quer.
 
-    }
-
-    public void diminuirVolume(){
-
-        volume --;
+        canal = novoCanal;
 
     }
 
 
 
+    public void aumentarCanal(){ // método para mudar de canal
+        canal++; // método unário para aumentar/mudar de canal
+    }
 
+    public void diminuirCanal(){
+        canal--;
+    }
 
+    // Void não me retorna nada
     public void ligar(){
-        ligada = true;
-
+        ligada = true; // TV está ligada
     }
-
+    
     public void desligar(){
-        ligada = false;
-        
+        ligada = false;// TV está ligada
     }
 
+
+    public void aumentarVolume(){ // O método aumentarVolume foi criado
+        // volume = volume + 1;
+        volume++; // add um operador unário para realizar essa ação
+        System.out.println("Aumentando o volume para: " + volume); // aumentando meu volume, estou imprimindo
+    
+    
+    }
+
+
+    public void diminuirVolume(){ // O método aumentarVolume foi criado
+        // volume = volume - 1;
+        volume--; // add um operador unário para realizar essa ação
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
 
 
 
--- tipos-variaveis/src/Usuario.java
+++ tipos-variaveis/src/Usuario.java
@@ -1,20 +1,37 @@
 public class Usuario {
-    public static void main(String[] args) throws Exception {
-        
-        SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV Ligada ? " +smartTv.ligada);
-        System.out.println("Canal Atual : " +smartTv.canal);
-        System.out.println("Volume Atual : " +smartTv.volume);
-    
-    
-        smartTv.ligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
-    
-        smartTv.desligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
-    
-    
-    
+    // Método Main (EXecuta nossa aplicação)
+    public static void main(String[] args) throws Exception {
+
+        // int, long, string =  (iteração de métodos, a minha classe é a SmartTv)
+        SmartTv smartTv = new SmartTv(); // criei uma nova classe
+
+
+        // Vou chamar atributos da classe Smart Tv e concatenar com a mensagem.
+        System.out.println("TV Ligada ? " +  smartTv.ligada); // comando para imprimir volume
+        System.out.println("Canal Atual : " + smartTv.canal); // comando para imprimir canal
+        System.out.println("Volume Atual : " + smartTv.volume); // comando para imprimir
+        
+        smartTv.ligar(); // chomar o método ligar
+        System.out.println("Novo status -> TV Ligada ? " +  smartTv.ligada);
+
+        smartTv.desligar(); // chomar o método desligar
+        System.out.println("Novo status -> TV Ligada ? " +  smartTv.ligada);
+
+        // Irei chamar o método para diminuir o volume
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume(); //esse método aumenta meu volume
+        System.out.println("Volume Atual : " + smartTv.volume); // imprimir o volume atual
+
+
+        System.out.println("Canal Atual : " + smartTv.canal); // estou imprimindo qual canal o usuário está
+
+        smartTv.mudarCanal(13); // executo o método canal com o número selecionado
+        System.out.println("Canal Atual : " + smartTv.canal); // chamo o método canal com a escolha do usuário
+
+
     }
+    
 }
